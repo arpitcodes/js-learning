1ca90d9aef9bf4bbc6d3712aec20d2cd1e8582ad
Arpit Mishra <aarpitmishraaa@gmail.com>
2024-02-20 13:39:29 +0530

    changes

--- 08_counter_program.js
+++
@@ -1,7 +0,0 @@
-//exercise to count till a certain value;
-
-const limit = 10;
-
-for(let i=1;i<=limit;i++){
-    console.log(`counter value is: ${i}`);
-}
--- 11_String_Slicing.js
+++
@@ -1,9 +0,0 @@
-const originalString = "JS is amazing!";
-const slicedSubstring = originalString.slice(0,10);
-// console.log(`sliced substring ( From 0 to 10 ): ${slicedSubstring}`);
-
-const endOfSentance = originalString.slice(-10);
-// console.log(endOfSentance);
-const subString = originalString.slice(0, originalString.length, 4);
-
-console.log(subString)
