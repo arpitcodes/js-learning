3cb0b9db56da999368420eb642c4eb6c01eb4de7
Arpit Mishra <aarpitmishraaa@gmail.com>
2024-02-20 13:39:28 +0530

    new modules added

---
+++ 10_string_methods.js
@@ -0,0 +1,23 @@
+const originalString = "hey there! today we will be learning string in js"
+const lowerCaseString = originalString.toLowerCase();
+const upperCaseString = originalString.toUpperCase();
+
+console.log(`original string: ${originalString}`);
+console.log(`upper case string: ${upperCaseString}`);
+console.log(`lower case string: ${lowerCaseString}`);
+console.log(`Length of the string: ${originalString.length}`)
+console.log(`substring calculation from the string: ${originalString.substring(5,10)}`)
+
+
+/**
+ * Other functions are as follows:
+ * trim
+ * concat
+ * split
+ * includes
+ * replace
+ * substring 
+ * etc
+ * 
+ * they all have there obivious names.
+ */
